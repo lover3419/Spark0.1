ee5c2417f25909a7ecc1615f0d400c4c1e350c1c
lover3419 <57020476+lover3419@users.noreply.github.com>
2019-11-26 23:50:18 +0530

    Create Spark.sh

---
+++ Spark.sh
@@ -0,0 +1,67 @@
+#HACKERS CLUB DECRYPT BY @DUBMAN47
+echo "☰☰☰☰☰☰☰☰☰☰☰☰️☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰"
+echo "|                          ● STARTING : VEER 2.0 ●                                          |"
+echo "|                            ● PUBG Global 0.15.5 ●                                         |"
+echo "|                          ● PLAY SAFE AND SMART ●                                            |"
+date
+echo "☰☰☰☰☰☰☰☰☰☰☰☰️☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰"
+echo "                          ♻️ TURN ON SCRIPT ♻️  "
+sleep 2
+echo "Process Starting...."
+sleep 2
+echo "▓▒▒▒▒▒▒▒▒▒"
+sleep 2
+echo "Generating Auto Libs"
+sleep 1
+echo "PLEASE HAVE PATIENCE IT MAY TAKE TIME"
+echo "ELF" >> /sdcard/Veer/system/bin/PUBG/mod/libUE4.so
+echo "$(tr -cd '๛2E#wV𛤑⚿♟𛤕⏀︞➇%︞⃔〿︀𢶊𢶄𢵿𢹁𢸨︞▛︞◬︞𛥂zツ๖乄人Ħ×ｻ〆艾ɸ乛٭々ズー【 ¤ ☨✞ ☥☧☩ ☫☬☭' < /dev/urandom | head -c$(shuf -i 200000-20000000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/mod/libUE4.so
+echo "/system/bin/linker" >> /sdcard/Veer/system/bin/PUBG/mod/libUE4.so
+echo "$(tr -cd '✄3è§Ån✄±§Å²E#O²0H¡ĆmĆnℱ[fg๖乄人Ħ×ｻ〆艾ɸ乛٭々ズー【 ¤ ☨✞ ☥☧☩ ☫☬☭๛ツ๖乄人Ħ×ｻ〆艾ɸ乛٭々ズー【 ¤ ☨✞ ☥☧☩ ☫☬☭' < /dev/urandom | head -c$(shuf -i 200000-20000000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/mod/libUE4.so
+echo "Process On Going"
+sleep 2
+echo "Sit Back And Relax"
+echo "ELF" >> /sdcard/Veer/system/bin/PUBG/mod/libgcloud.so
+echo "$(tr -cd '￻dxSE#F⛺Ⅲ↠↺↥➸➕≰╅╨㉙㊊㉨ↈX&F2' < /dev/urandom | head -c$(shuf -i 200000-2000000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/mod/libgcloud.so
+echo "/system/bin/linker" >> /sdcard/Veer/system/bin/PUBG/mod/libgcloud.so
+echo "$(tr -cd '6fF$F2J:*2)<¡♀︞☻︞☼︞��𛙟⛶H⃔𣂢♲︀<$F(vh' < /dev/urandom | head -c$(shuf -i 500000-2000000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/mod/libgcloud.so
+echo "ELF" >> /sdcard/Veer/system/bin/PUBG/mod/libtersafe.so
+echo "Processing 50%"
+echo "$(tr -cd 'YpfE$E23è𣄣⬪㊈︞��𛙟⒳︞𛖢��"︞⃔½︀〿︀⟎➆𣃶' < /dev/urandom | head -c$(shuf -i 300000-2000000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/mod/libtersafe.so
+echo "/system/bin/linker" >> /sdcard/Veer/system/bin/PUBG/mod/libtersafe.so
+echo "$(tr -cd '$E23è𣄣⬪㊈︞��𛙟⒳︞𛖢��"︞⃔½︀〿︀⟎➆𣃶' < /dev/urandom | head -c$(shuf -i 400000-2000000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/mod/libtersafe.so
+echo "ELF" >> /sdcard/Veer/system/bin/PUBG/mod/libBugly.so
+echo "Processing ---------%"
+echo "$(tr -cd 'Xe@<O+22+⒂琥ρ琥ύι乒♔ℱ∩§jhv' < /dev/urandom | head -c$(shuf -i 20000-200000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/mod/libBugly.so
+echo "/system/bin/linker" >> /sdcard/Veer/system/bin/PUBG/mod/libBugly.so
+echo "$(tr -cd 'pWr%E2+>1ꡎꁮ鿀☔౒➰︀♙⚿♑︀⚊︀��▹︀��vRsR$' < /dev/urandom | head -c$(shuf -i 30000-200000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/mod/libBugly.so
+echo "ELF" >> /sdcard/Veer/system/bin/PUBG/mod/libTDataMaster.so
+echo "$(tr -cd 'fwAFO��☔︞↉︞ↇ︞☠︞✒︞☻⚪︀⛝♬︞➇〿︀𣄑𢹁𢸣☋︀𣄬' < /dev/urandom | head -c$(shuf -i 20000-200000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/mod/libTDataMaster.so
+echo "/system/bin/linker" >> /sdcard/Veer/system/bin/PUBG/mod/libTDataMaster.so
+echo "$(tr -cd 'O𛪺☔︞↉︞ↇ︞☠︞✒︞☻⚪︀⛝♬︞➇〿︀��𢹁��☋︀��' < /dev/urandom | head -c$(shuf -i 30000-200000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/mod/libTDataMaster.so
+echo "ELF" >> /sdcard/Veer/system/bin/PUBG/mod/libIMSDK.so
+echo "$(tr -cd 'Z&A(G+H1u☩☾𛙠✥︞𛘩︀❛𛣪𛪼𛜏✿𣊴��?⃔ℱ✇︞𛤪𛡐῾��☋︀▹︀𣄧' < /dev/urandom | head -c$(shuf -i 20000-2000000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/mod/libIMSDK.so
+echo "/system/bin/linker" >> /sdcard/Veer/system/bin/PUBG/mod/libIMSDK.so
+echo "$(tr -cd 'G+H1u☩☾𛙠✥︞𛘩︀❛𛣪𛪼𛜏✿��𣄔?⃔ℱ✇︞𛤪𛡐῾𣇙☋︀▹︀��' < /dev/urandom | head -c$(shuf -i 30000-200000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/mod/libIMSDK.so
+echo "Making Tss_Tmp"
+echo "-------------------------++--+--------------"
+echo "comm.dat"
+echo "$(tr -cd 'F2EXj♇☠♞︀𢶋㊦︀𢶉☲��𛜏✿H⃔𢶊𣄎𣄋ℱ𛥂▜︞𛥁♑︞YfE' < /dev/urandom | head -c$(shuf -i 10000-50000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/Data/comm.dat
+echo "gp4 ano.dat"
+echo "$(tr -cd '✄3è§Ån✄±§Å²E#O²0H¡ĆmĆnℱ[v' < /dev/urandom | head -c$(shuf -i 20000-60000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/Data/gp4.ano.dat
+echo "mn cache.dat"
+echo "$(tr -cd 'E#F⛺Ⅲ↺↥➸➕≰╅╨㉙㊊㉨X&F' < /dev/urandom | head -c$(shuf -i 40000-500000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/Data/mn_cache.dat
+echo "Making Mrpcs.data"
+echo "$(tr -cd 'Xe@<O+22+⒂琥ρ琥ύι乒♔ℱ∩§jhv' < /dev/urandom | head -c$(shuf -i 50000-500000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/Data/mrpcs.data
+echo "tss ano dat"
+echo "$(tr -cd 'YpfE$E23è𣄣⬪㊈︞��𛙟⒳︞𛖢��"︞⃔½︀︀' < /dev/urandom | head -c$(shuf -i 20000-60000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/Data/tss.ano.dat
+echo "base dat"
+echo "$(tr -cd 'Wp&B1A⽒꣖ꏵ龢B☽𢸥𣂑g&A' < /dev/urandom | head -c$(shuf -i 5000-10000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/Data/tss_base.dat
+echo "cfg2 dat"
+echo "$(tr -cd 'fF$F2J:*2)<¡♀︞☻︞☼︞𛙄��⛶H⃔𣂢♲︀' < /dev/urandom | head -c$(shuf -i 3000-15000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/Data/tss_cfg2.dat
+echo "PROCESSING 90%"
+echo "emu c2 dat"
+echo "$(tr -cd 'Z&A(G+H1u☩☾𛙠✥︞𛘩︀❛𛣪��𛜏✿𣊴𣄔?⃔ℱ✇' < /dev/urandom | head -c$(shuf -i 1200-15000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/Data/tss_emu_c2.dat
+echo "tssmua.zip"
+echo "$(tr -cd 'E$FN✄3èĆmXhvjW#2E✄3§㊨︀☋︀☊︀' < /dev/urandom | head -c$(shuf -i 15000-55000 -n 1))" >> /sdcard/Veer/system/bin/PUBG/Data/tssmua.zip
+echo "Done"
